d261a46f8c8383bfb9b6ca730be819c4487dd177
curtis <curtis@edticket.com>
2017-06-01 19:26:23 +0900

    add setting

--- tayo/settings.py
+++ tayo/settings.py
@@ -97,7 +97,9 @@ AUTH_PASSWORD_VALIDATORS = [
 CRONJOBS = [
     ('01 23 * * *', 'schedule.cron.store_historyschedule'),
     ('00 21 * * *', 'schedule.cron.store_historyschedule_old'),
-    ('00 22 * * sun', 'schedule.cron.reset_lflag_on_every_schedule')
+    ('00 22 * * sun', 'schedule.cron.reset_lflag_on_every_schedule'),
+    ('00 23 31 12 *', 'schedule.test.move_schedule'),
+    ('00 23 31 12 *', 'schedule.test.regist_student')
 ]
 
 #CRONTAB_COMMAND_SUFFIX = '2>&1'
