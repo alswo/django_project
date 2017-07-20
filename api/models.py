33c52e4a5332d7ccfbacdaf4b6598197b3e81691
curtis <curtis@edticket.com>
2017-07-20 20:54:27 +0900

    add notices api

--- api/models.py
+++ api/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+def getNow():
+	return timezone.localtime(timezone.now())
+
+class Notice(models.Model):
+	title = models.CharField(max_length=50)
+	datetime = models.DateTimeField(default=timezone.now)
+	content = models.TextField()
