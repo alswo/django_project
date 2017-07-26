7263867d46cbffb93dab69c38870afc4087d749f
curtis <curtis@edticket.com>
2017-07-26 16:38:02 +0900

    move cron setting to local_settings.py

--- tayo/settings.py
+++ tayo/settings.py
@@ -93,7 +93,6 @@ CRONJOBS = [
     ('00 21 * * *', 'schedule.cron.store_historyschedule_old'),
     ('00 22 * * sun', 'schedule.cron.reset_lflag_on_every_schedule'),
     ('30 23 * * sun', 'schedule.cron.weekly_update'),
-    ('30 12 * * *', 'schedule.cron.find_update')
     #('00 23 31 12 *', 'schedule.test.move_schedule'),
     #('00 23 31 12 *', 'schedule.test.regist_student')
 ]
