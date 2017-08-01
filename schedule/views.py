96f9ea65dc56681975ab96f7ddda329cc9d71644
leejinhlee <cto@edticket.com>
2017-08-01 13:32:32 +0900

    bug patch by rocky

--- schedule/views.py
+++ schedule/views.py
@@ -168,7 +168,7 @@ def getSchedule(request):
                 return render_to_response('getSchedule.html', {"contacts": contacts, "bid" : bid, "aid" : aid,'user':request.user})
 
         elif request.user.groups.filter(name__in = ['driver']).exists():
-            branch = Car.objects.get(id = car)
+            branch = Car.objects.get(carname = car)
             invens = Inventory.objects.filter(carnum=car).filter(day = day)
 
             contacts = []
