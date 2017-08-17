1331825a11de5dab6ecb681503c88a354b8bda8f
curtis <curtis@edticket.com>
2017-08-17 23:56:55 +0900

    personInfoUtil

--- util/PersonalInfoUtil.py
+++ util/PersonalInfoUtil.py
@@ -2,8 +2,17 @@
 from passenger.models import StudentInfo, PersonalInfo
 from schedule.models import Branch
 from django.utils.crypto import get_random_string
+import re
 
-def compareLists(item1, list1, item2, list2):
+def getHangul(str):
+	hangul = re.compile('^[\u3131-\u3163\uac00-\ud7a3]+')
+	m = hangul.match(str)
+	return m.group()
+
+def compareLists(name1, item1, list1, name2, item2, list2):
+        if (getHangul(name1) != getHangul(name2)):
+		return False
+
 	if ((len(str(item1)) < 9) or (len(str(item2)) < 9)):
 		return False
 
@@ -27,7 +36,7 @@ def compareLists(item1, list1, item2, list2):
 	return not set(compList1).isdisjoint(compList2)
 
 def findSamePerson(student1, student2):
-	if (student1.bid == student2.bid and student1.sname == student2.sname):
+	if (student1.bid == student2.bid and getHangul(student1.sname) == getHangul(student2.sname)):
 		list1 = []
 		list1.append(student1.parents_phonenumber)
 		list1.append(student1.grandparents_phonenumber)
@@ -68,7 +77,7 @@ def saveNewPersonInfo(student):
 			another = StudentInfo.objects.get(personinfo = person, bid = student.bid)
 		except :
 			continue
-		if compareLists(student.phone1, student.phonelist, another.phone1, another.phonelist):
+		if compareLists(student.sname, student.phone1, student.phonelist, another.sname, another.phone1, another.phonelist):
 			pin_number = person.pin_number
 			break
 
@@ -83,12 +92,13 @@ def saveNewPersonInfo2(student):
 	pin_number = get_random_string(length=7)
 	for person in people:
 		try : 
-			another = StudentInfo.objects.get(personinfo = person, bid = student.bid)
+			otherStudents = StudentInfo.objects.filter(personinfo = person, bid = student.bid)
 		except :
 			continue
-		if findSamePerson(student, another):
-			pin_number = person.pin_number
-			break
+		for otherStudent in otherStudents:
+			if findSamePerson(student, otherStudent):
+				pin_number = person.pin_number
+				break
 
 	siblings = StudentInfo.objects.filter(bid = student.bid)
 	for sibling in siblings:
