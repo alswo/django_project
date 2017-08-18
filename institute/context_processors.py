1322dcfd0717509e0dd49bfe5249c9fdfb0b6451
curtis <curtis@edticket.com>
2017-08-18 18:05:49 +0900

    add some variables

--- institute/context_processors.py
+++ institute/context_processors.py
@@ -4,6 +4,6 @@ def getAcademy(request):
 	if request.user.is_active :
 		academies = Academy.objects.all().order_by('name')
 		displayname = request.session.get('institute', request.user.first_name)
-		return {'academies' : academies, 'displayname' : displayname}
+		return {'academies' : academies, 'displayname' : displayname, 'age_range': range(5, 16), 'billing_range': range(1, 32), 'month_range': range(1, 13)}
 
 	return {}
