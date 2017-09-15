f2fe321a244bb25deb6a599ac693bd5440c51b1c
curtis <curtis@edticket.com>
2017-09-15 16:41:17 +0900

    move to route2

--- optimizer/client.py
+++ optimizer/client.py
@@ -7,7 +7,7 @@ import json
 #algorithm = ['prim', 'salesman', 'onlytime', 'mintime']
 algorithm = ['salesman']
 params = {}
-url = "http://curtis-tayotayo.edticket.com/optimizer/getRoute"
+url = "http://route2-tayotayo.edticket.com/optimizer/getRoute"
 #url = "http://curtis-tayotayo.edticket.com/optimizer/getRouteSequential"
 #data = {'startName': '낙원중학교', 'startX':'14148317.661607', 'startY':'4494878.084352', 'endName':'원마을현대힐스테이트', 'endX':'14148219.329390', 'endY':'4494726.671574', 'viaPoints': [{ 'viaPointName':'낙생고등학교', 'viaPointX':'14148809.322692', 'viaPointY':'4493197.096773'}, {'viaPointName':'판교도서관', 'viaPointX':'14147628.099206', 'viaPointY':'4493893.745713'}]}
 #data = {'startName': 'A', 'startX':'14148317.661607', 'startY':'4494878.084352', 'endName':'B', 'endX':'14148219.329390', 'endY':'4494726.671574', 'viaPoints': [{ 'viaPointName':'C', 'index':1, 'viaPointX':'14148809.322692', 'viaPointY':'4493197.096773', 'viaPointNumPassenger': 1}, {'viaPointName':'D', 'index':0, 'viaPointX':'14147628.099206', 'viaPointY':'4493893.745713', 'viaPointNumPassenger': 30}]}
