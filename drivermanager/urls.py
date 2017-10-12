010d890e8ff5e839b33bdb6197d615ff503b92f6
leejinhee <jiny8964@gmail.com>
2017-10-12 19:56:24 +0900

    driverview carSchedule

--- drivermanager/urls.py
+++ drivermanager/urls.py
@@ -4,6 +4,7 @@ from drivermanager import views
 urlpatterns = [
     url(r'^main', views.get_drivermanager_page),
     url(r'^getSchedule', views.get_schedule),
+    url(r'^getCarSchedule', views.car_schedule),
 ]
 
 
--- drivermanager/views.py
+++ drivermanager/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core import serializers
 from passenger.models import Profile, StudentInfo
-from schedule.models import Inventory, ScheduleTable, Branch
+from schedule.models import Inventory, ScheduleTable, Branch, Area, Car
 import datetime
 from collections import defaultdict
 from django.views.decorators.csrf import csrf_exempt
@@ -60,6 +60,50 @@ def day_schedule(bid, day):
         contacts.append(temp_dict)
 
     return contacts
+
+def car_schedule(car):
+    invens = Inventory.objects.filter(carnum = day)
+   
+    temp_car = []
+
+    for inven in invens:
+        temp_car.append(inven.carnum)
+    
+    temp_car = set(temp_car)
+
+    contacts = []
+    for tc in temp_car:
+        temp_dict = {}
+        temp_dict['carnum'] = tc
+        
+        temp_inven_list = []
+        for inven in invens:
+            temp_invens = {}
+            if inven.carnum == tc:
+                temp_invens['stime'] = inven.stime
+		temp_invens['etime'] = inven.etime
+                temp_invens['snum'] = inven.snum
+                temp_invens['iid'] = inven.id
+                temp_invens['aname'] = inven.anamelist
+                temp_invens['day'] = inven.day
+                
+                for studentInfo in inven.slist:
+                    try:
+                        sInfo = StudentInfo.objects.get(id = studentInfo)
+                    except StudentInfo.DoesNotExist:
+                        continue
+                    if sInfo.birth_year == None or ((datetime.datetime.now().year - int(sInfo.birth_year) + 1) <= 13):
+                        temp_invens['passenger'] = 0
+                        break
+
+                temp_inven_list.append(temp_invens)
+                
+
+        temp_dict['invens'] = temp_inven_list
+        
+        contacts.append(temp_dict)
+
+    return contacts  
     
 @login_required
 @user_passes_test(is_not_drivermanager, login_url='/', redirect_field_name=None)
@@ -108,5 +152,59 @@ def get_schedule(request):
         else:
             pass 
         return HttpResponse(json.dumps(contacts))
+
+@login_required
+@user_passes_test(is_not_drivermanager, login_url='/', redirect_field_name=None)
+@csrf_exempt
+def car_schedule(request):
+    weekdaylist = ['월', '화', '수', '목', '금', '토']
+
+    if request.method == 'GET':
+        if request.user.is_superuser:
+            if request.GET.get('aid'):
+                aid = int(request.GET.get('aid'))
+            else:
+                aid = -1
+
+            if request.GET.get('bid'):
+                bid = int(request.GET.get('bid'))
+            else:
+                bid = -1
+
+            if request.GET.get('car'):
+                car = int(request.GET.get('car'))
+            else:
+                car = -1
+
+            area = Area.objects.all() 
+            if aid > 0:
+                if bid > 0:
+                    branch = Branch.objects.filter(areaid=aid)
+                    car = Car.objects.filter(branchid=bid)
+
+                    return render_to_response('carSchedule.html', {'area':area, 'branch':branch,'car':car, 'aid':aid, 'bid':bid, 'user':request.user}) 
+                
+                else:
+                    branch = Branch.objects.filter(areaid=aid)
+
+                    return render_to_response('carSchedule.html', {'area':area, 'branch':branch, 'aid':aid, 'user':request.user})    
+
+            else:
+                return render_to_response('carSchedule.html', {'area':area, 'user':request.user})            
+ 
+        else:
+            areaid = Profile.objects.get(user_id = request.user).bid 
+            branch = Branch.objects.filter(areaid = areaid)
+ 
+            return render_to_response('carSchedule.html', {'branch':branch, 'user':request.user})
+
+        return render_to_response('carSchedule.html')  
+
+    if request.method == "POST":
+        car = int(request.POST.get('car'))
+        
+        contacts = car_schedule(car)
+        
+        return HttpReponse(json.dumps(contacts)) 
 
 
