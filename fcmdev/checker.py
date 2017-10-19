c2de9dc069452abd9beaa50ceaae22a346309fe6
alswo <apep1111@naver.com>
2017-10-19 20:02:18 +0900

    copied:     fcmdev/tasks.py -> fcmdev/checker.py 	modified:   fcmdev/tasks.py 	modified:   passenger/templates/passenger/main.html

--- fcmdev/checker.py
+++ fcmdev/checker.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from tayo.celery import app
 from passenger.models import StudentInfo, PersonalInfo
 from passenger.dateSchedule import timeToDate
 from fcmdev.models import PropOfDevice
@@ -12,122 +14,43 @@ import simplejson
 import ast
 reload(sys)
 sys.setdefaultencoding('utf-8')
-#
-#
-# def check():
-#     time = timeToDate()
-#     date = time.timeToD()
-#     i = 0
-#     scheduleTables = []
-#     schedules = []
-#     inventorys = Inventory.objects.filter(day = date).prefetch_related('scheduletables').reverse()
-#     for inventory in inventorys:
-#         scheduletables = ScheduleTable.objects.filter(iid = 2956)
-#         for scheduletable in scheduletables:
-#             schedules.extend(scheduletable.slist)
-#
-#     for inventory in inventorys:
-#         scheduleTables.extend(inventory.scheduletables.all())
-#
-#     count_slist = []
-#
-#     for schTable in scheduleTables:
-#         if len(schTable.slist) >= 1:
-#             count_slist.extend(schTable.slist)
-#
-#     dict_slist = dict(Counter(count_slist))
-#
-#     push_content = []
-#
-#     msg_count = 0
-#     error_count = 0
-#     for key, value in dict_slist.iteritems():
-#         for schTable in scheduleTables:
-#             if len(schTable.slist) >= 1:
-#                 while(key in schTable.slist):
-#                     try:
-#                         sInfo = StudentInfo.objects.get(id=key)
-#                     except StudentInfo.DoesNotExist:
-#                         sInfo = None
-#                         break
-#                     else:
-# 			try:
-#
-# 				pin = PersonalInfo.objects.get(id = sInfo.personinfo_id)
-# 	                        module_push_content = {}
-# 				# 안타요를 누른 학생에대해서 메시지 필터링.
-# 				temp_tflag = schTable.tflag
-# 				offset_list = schTable.slist
-# 				temp_index = offset_list.index(sInfo.id)
-# 				module_push_content['tflag'] = temp_tflag[temp_index]
-#                                 module_push_content['id'] =schTable.id
-# 				module_push_content['sname'] = sInfo.sname
-# 	                        module_push_content['time'] = schTable.time
-# 	                        module_push_content['addr'] = schTable.addr
-# 		                module_push_content['lflag'] = schTable.lflag
-# 	                        module_push_content['aname'] = sInfo.aname
-# 	                        module_push_content['pin'] = pin.pin_number
-# 	                        module_push_content['sid'] = key
-# 	                        module_push_content['count'] = value
-#
-# 	                        push_content.append(module_push_content)
-# 	                        break
-# 			except PersonalInfo.DoesNotExist:
-# 			        break
-#
-#
-#
-#         sctid = module_push_content['id']
-#         count = module_push_content['count']-1
-# 	lflag = module_push_content['lflag']
-# 	sname = module_push_content['sname']
-#         tflag = module_push_content['tflag']
-#         if lflag == 0:
-#             flag = "하원을 위한"
-#
-# 	elif lflag == 1:
-# 	    flag = "등원을 위한"
-# 	else:
-# 	    flag = "에 대한"
-#         if count == 0:
-# 	    i += 1
-#             msg = "오늘 " + sname + " 학생의 " + module_push_content['aname'] + " " + flag + " " + module_push_content['time'] + " [" + module_push_content['addr'] + "] 승차 스케줄이 있습니다"
-#
-# 	    # if tflag == 1:
-#         #         print sctid
-# 	    # 	print sname+"안타요"
-# 	    # elif tflag == 0:
-# 	    #     print sctid
-#         #         print sname+"타요"
-#         else:
-# 	    i += 1
-#             msg = "오늘 " + sname + " 학생의 " + module_push_content['aname'] + " 등원을 위한 " + module_push_content['time'] + " [" + module_push_content['addr'] + "]승차 외" + str(count) + "건의 스케줄이 있습니다."
-#
-#     print i
-#
-#
-# def check2():
-#     scheduletables = ScheduleTable.objects.filter(iid = 2956)
-#     for sc in scheduletables:
-#         for slist in sc.slist:
-#             sInfo = StudentInfo.objects.get(id=1592)
-#
-#         # temp_tflag = sc.tflag
-#         # offset_list = sc.slist
-#         # temp_index = offset_list.index(sInfo.id)
+
+def getResponse(debug, code, msg):
+	if (debug == 1):
+		return HttpResponse(msg)
+	else:
+		return JsonResponse({'code': code, 'msg': msg})
 
 
+@app.task
 def tt():
     time = timeToDate()
     date = time.timeToD()
-    i = 0
+    si = []
     scheduleTables = []
     schedules = []
+	slist_list = []
+    tflag_list = []
+    dict_s ={}
     inventorys = Inventory.objects.filter(day = date).prefetch_related('scheduletables').reverse()
     for inventory in inventorys:
         scheduletables = ScheduleTable.objects.filter(iid = inventory.id)
         for scheduletable in scheduletables:
             schedules.extend(scheduletable.slist)
+			slist_list.extend(scheduletable.slist)
+		    tflag_list.extend(scheduletable.tflag)
+
+	for s in slist_list:
+        try:
+            si =  slist_list.index(s)
+        except:
+            si = None
+        else:
+            if dict_s.has_key(s):
+                dict_s[s] += int(tflag_list[si])
+            else:
+                 dict_s[s] = int(tflag_list[si])
+    print dict_s
 
     for inventory in inventorys:
         scheduleTables.extend(inventory.scheduletables.all())
@@ -142,8 +65,6 @@ def tt():
 
     push_content = []
 
-    msg_count = 0
-    error_count = 0
     for key, value in dict_slist.iteritems():
         for schTable in scheduleTables:
             if len(schTable.slist) >= 1:
@@ -158,13 +79,7 @@ def tt():
 
 				pin = PersonalInfo.objects.get(id = sInfo.personinfo_id)
 	                        module_push_content = {}
-				# 안타요를 누른 학생에대해서 메시지 필터링.
-				# temp_tflag = schTable.tflag
-				# offset_list = schTable.slist
-				# temp_index = offset_list.index(sInfo.id)
-				# module_push_content['tflag'] = temp_tflag[temp_index]
-                #                 module_push_content['id'] =schTable.id
-				module_push_content['sname'] = sInfo.sname
+	                        module_push_content['sname'] = sInfo.sname
 	                        module_push_content['time'] = schTable.time
 	                        module_push_content['addr'] = schTable.addr
 		                module_push_content['lflag'] = schTable.lflag
@@ -172,7 +87,6 @@ def tt():
 	                        module_push_content['pin'] = pin.pin_number
 	                        module_push_content['sid'] = key
 	                        module_push_content['count'] = value
-
 	                        push_content.append(module_push_content)
 	                        break
 			except PersonalInfo.DoesNotExist:
@@ -180,14 +94,12 @@ def tt():
 
 
 
-        sctid = module_push_content['id']
+
         count = module_push_content['count']-1
-        # tflag = module_push_content['tflag']
 	lflag = module_push_content['lflag']
 	sname = module_push_content['sname']
-        # if tflag == '1':
-        #     count = count - 1
-        if lflag == 0:
+
+	if lflag == 0:
             flag = "하원을 위한"
 
 	elif lflag == 1:
@@ -196,23 +108,47 @@ def tt():
 	    flag = "에 대한"
         if count == 0:
             msg = "오늘 " + sname + " 학생의 " + module_push_content['aname'] + " " + flag + " " + module_push_content['time'] + " [" + module_push_content['addr'] + "] 승차 스케줄이 있습니다"
+            send_msg(module_push_content['sid'], module_push_content['pin'], msg)
 
-
-            #send_msg(module_push_content['sid'], module_push_content['pin'], msg)
-	    # if tflag == 1:
-        #         print msg
-	    # 	print sname+"안타요"
-        #
-        #     elif tflag == 0:
-	    #     pass
-
-        elif count > 0:
+        else:
             msg = "오늘 " + sname + " 학생의 " + module_push_content['aname'] + " 등원을 위한 " + module_push_content['time'] + " [" + module_push_content['addr'] + "]승차 외" + str(count) + "건의 스케줄이 있습니다."
-            print msg
+            send_msg(module_push_content['sid'], module_push_content['pin'], msg)
 
-           # send_msg(module_push_content['sid'], module_push_content['pin'], msg)
-            # if tflag == 1:
-            #     print msg
-            #     print sname+"안타요"
-            # elif tflag == 0:
-            #     pass
+
+def test():
+	print "test"
+
+def send_msg(sid, pin, msg):
+    url = 'https://fcm.googleapis.com/fcm/send'
+    header = {'authorization': 'key=AAAAWVvmwNU:APA91bH0IjidQtMmX6q9SRVekZqzNmWKRR15mdjOFFAt05v3E7PziYRb7sLMbtCtNXZYyKrz--fKvoZdDY94yjOrH9G6z-axN7qWS7H5VMBRUy8Z6-dysdj9ZaCYrESl2wnIfOoSnh7X','content-type': 'application/json'}
+    result = {}
+
+    prop = PropOfDevice.objects.filter(pin_number = pin)
+    for p in prop:
+        pushcheck = p.receivePush
+        fcm = FCMDevice.objects.filter(device_id = p.device_id)
+        for f in fcm:
+            token = f.registration_id
+            types = f.type
+
+            if pushcheck == False:
+                print ("he/she doesn't want to receive push message.")
+            else:
+                if types == 'android':
+                    payload = '{\n    "to" : "' + str(token) + '","priority" : "high", "content-available" : "true","collapse_key" : "Updates Available" ,"notification": {\t  "body" : "'+str(msg)+'","title" : "셔틀타요", "sound":"default"},\t}'
+                elif types == 'ios':
+                    payload = '{\n    "to" : "' + str(token) + '","priority" : "high", "content-available" : "true","collapse_key" : "Updates Available" ,"notification": {\t  "body" : "'+str(msg)+'", "sound":"default"},\t}'
+                try:
+                    sid = str(sid)
+                    response = requests.request('POST', url, data=payload, headers=header)
+                    try:
+                        result = ast.literal_eval(response.text)
+                        status = str(result['success'])
+                        pushurl = 'http://api.edticket.com/fcmdev/pushConfirmInfo'
+                        data = "pin="+pin+"&confirming="+response.text+"&status="+status+"&token="+token+"&sid="+sid
+                        headers = {'content-type': "application/x-www-form-urlencoded"}
+                        response = requests.request("POST", pushurl, data=data, headers=headers)
+                    except:
+                        print "msg check error"
+                except:
+                    print ("msg send error")
